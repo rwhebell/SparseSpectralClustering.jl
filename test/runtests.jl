87bbf8ac4e04e205f03ed46d4e10531786ee126d
Riley Whebell <75467572+rwhebell@users.noreply.github.com>
2024-12-10 18:02:46 +1000

    Make test less prone to random failure

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,9 @@ using LinearAlgebra, SparseArrays
 
 @testset "SparseSpectralClustering.jl small" begin
     k = 3
-    X = [ rand(3,300) 1.1.+rand(3,300) 2.8.+3.0.*rand(3,400) ]
+    X = [ rand(3,300) 1.5.+rand(3,300) 2.8.+3.0.*rand(3,400) ]
     S = [ exp( -norm(X[:,i]-X[:,j])^2 / 0.2^2 ) for i in 1:1000, j in 1:1000 ]
-    S = fkeep!((i,j,sij) -> sij > 1e-8, sparse(S))
+    S = fkeep!((i,j,sij) -> sij > 1e-12, sparse(S))
     idxs = spectralcluster(S, k)
     @test allequal(idxs[1:300])
     @test allequal(idxs[301:600])
@@ -16,7 +16,7 @@ end
 
 @testset "SparseSpectralClustering.jl large" begin
     k = 3
-    X = [ rand(3,30000) 1.1.+rand(3,30000) 2.8.+3.0.*rand(3,40000) ]
+    X = [ rand(3,30000) 1.5.+rand(3,30000) 2.8.+3.0.*rand(3,40000) ]
     S = knnSimilarity(X, 10, 0.2)
     idxs = spectralcluster(S, k)
     @test allequal(idxs[1:30000])
