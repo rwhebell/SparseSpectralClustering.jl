d0b9fcb2aaeb5e93151ea5bba5bf2b4297513871
Riley Whebell <75467572+rwhebell@users.noreply.github.com>
2024-12-10 18:14:38 +1000

    Tweak test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,9 @@ using LinearAlgebra, SparseArrays
 
 @testset "SparseSpectralClustering.jl small" begin
     k = 3
+    σ = 0.1
     X = [ rand(3,300) 1.5.+rand(3,300) 2.8.+3.0.*rand(3,400) ]
-    S = [ exp( -norm(X[:,i]-X[:,j])^2 / 0.2^2 ) for i in 1:1000, j in 1:1000 ]
+    S = [ exp( -norm(X[:,i]-X[:,j])^2 / σ^2 ) for i in 1:1000, j in 1:1000 ]
     S = fkeep!((i,j,sij) -> sij > 1e-12, sparse(S))
     idxs = spectralcluster(S, k)
     @test allequal(idxs[1:300])
@@ -16,8 +17,9 @@ end
 
 @testset "SparseSpectralClustering.jl large" begin
     k = 3
+    σ = 0.1
     X = [ rand(3,30000) 1.5.+rand(3,30000) 2.8.+3.0.*rand(3,40000) ]
-    S = knnSimilarity(X, 10, 0.2)
+    S = knnSimilarity(X, 10, σ)
     idxs = spectralcluster(S, k)
     @test allequal(idxs[1:30000])
     @test allequal(idxs[30001:60000])
