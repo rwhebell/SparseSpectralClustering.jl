22f56c11093a632792cea53e407d0c419d4c76df
Riley Whebell <75467572+rwhebell@users.noreply.github.com>
2024-12-10 17:58:34 +1000

    Tidy tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using SparseSpectralClustering
 using Test
 using LinearAlgebra, SparseArrays
 
-@testset "SparseSpectralClustering.jl" begin
+@testset "SparseSpectralClustering.jl small" begin
     k = 3
     X = [ rand(3,300) 1.1.+rand(3,300) 2.8.+3.0.*rand(3,400) ]
     S = [ exp( -norm(X[:,i]-X[:,j])^2 / 0.2^2 ) for i in 1:1000, j in 1:1000 ]
@@ -12,10 +12,12 @@ using LinearAlgebra, SparseArrays
     @test allequal(idxs[301:600])
     @test allequal(idxs[601:end])
     @test allunique(idxs[[1,301,601]])
+end
 
+@testset "SparseSpectralClustering.jl large" begin
     k = 3
     X = [ rand(3,30000) 1.1.+rand(3,30000) 2.8.+3.0.*rand(3,40000) ]
-    S = knnSimilarity(X, 10)
+    S = knnSimilarity(X, 10, 0.2)
     idxs = spectralcluster(S, k)
     @test allequal(idxs[1:30000])
     @test allequal(idxs[30001:60000])
