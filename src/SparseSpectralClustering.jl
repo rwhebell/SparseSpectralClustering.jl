d1a8cb869b086769cd8869b5f806ef70604e4919
Riley Whebell <75467572+rwhebell@users.noreply.github.com>
2024-12-10 17:47:11 +1000

    First functionality

--- src/SparseSpectralClustering.jl
+++ src/SparseSpectralClustering.jl
@@ -1,5 +1,70 @@
 module SparseSpectralClustering
 
-# Write your package code here.
+using LinearAlgebra, Arpack, Clustering, SparseArrays, Distances, NearestNeighbors
+
+export spectralcluster, knnSimilarity
+
+""" 
+idxs = spectralcluster(S, k; method=:kmedoids)
+- `S` is the n×n similarity matrix (should be sparse, especially if large)
+- `k` is the number of clusters desired
+
+kmeans is used on the eigenvectors of the normalized Laplacian
+
+- `idxs` is a vector of `Int`s that give cluster numbers for each point
+"""
+function spectralcluster(S, k)
+
+    @assert size(S,1) == size(S,2) "S should be square."
+
+    # S = Symmetric(S)
+
+    d = getDegree(S)
+    L = makeNormalizedLaplacian(S, d)
+
+    # this is the sparse, approximate eigenpair finder from Arpack
+    λ, v, _ = eigs(L; nev=k, ritzvec=true, which=:SM, maxiter=10_000, tol=1e-4)
+
+    v = real.(v)
+
+    # normalise to unit length because we used the symmetric normalized Laplacian
+    v ./= norm.(eachrow(v))
+    # transpose to pass to clustering with rows as points
+    v = transpose(v)
+
+    clustering = kmeans(v, k)
+
+    return assignments(clustering)
+
+end
+
+function getDegree(S)
+    n = size(S, 1)
+    return S * ones(eltype(S), n)
+end
+
+function makeNormalizedLaplacian(S, d)
+    # Ng-Jordan-Weiss: symmetric normalized Laplacian
+    inv_sqrt_D = sparse(Diagonal(d .^(-1/2)))
+    return (I - inv_sqrt_D * S * inv_sqrt_D)
+    # equivalent to inv_sqrt_D * (D - S) * inv_sqrt_D
+end
+
+function knnSimilarity(X, m, σ)
+    n = size(X,2)
+    kdtree = KDTree(X)
+    Si = Int[]
+    Sj = Int[]
+    Sv = eltype(X)[]
+    for i in 1:n
+        J, dists = knn(kdtree, X[:,i], m)
+        for (j, d) in zip(J, dists)
+            push!(Si, i)
+            push!(Sj, j)
+            push!(Sv, exp(-d^2/σ^2))
+        end
+    end
+    return SparseArrays.sparse!(Si, Sj, Sv, n, n, max)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using SparseSpectralClustering
 using Test
+using LinearAlgebra, SparseArrays
 
 @testset "SparseSpectralClustering.jl" begin
-    # Write your tests here.
+    k = 3
+    X = [ rand(3,300) 1.1.+rand(3,300) 2.8.+3.0.*rand(3,400) ]
+    S = [ exp( -norm(X[:,i]-X[:,j])^2 / 0.2^2 ) for i in 1:1000, j in 1:1000 ]
+    S = fkeep!((i,j,sij) -> sij > 1e-8, sparse(S))
+    idxs = spectralcluster(S, k)
+    @test length(unique(idxs[1:300])) == 1
+    @test length(unique(idxs[301:600])) == 1
+    @test length(unique(idxs[601:end])) == 1
+    @test allunique(idxs[[1,301,601]])
 end
